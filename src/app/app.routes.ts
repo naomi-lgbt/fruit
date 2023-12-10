ba809dadd0bf2289bd6e1c6eac72c34156930fdd
Naomi Carrigan <nhcarrigan@gmail.com>
2023-12-09 17:47:44 -0800

    feat: styling when navlink route is active

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,8 @@ import { BlogComponent } from './blog/blog.component';
 import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'blog', component: BlogComponent, pathMatch: 'full' },
   { path: 'blog/:slug', component: BlogComponent },
